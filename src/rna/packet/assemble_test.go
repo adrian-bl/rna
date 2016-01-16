5e7f191d81a785a6daebe9d7892806de6e9cbbaa
Adrian Ulrich <adrian@blinkenlights.ch>
2016-01-16 09:36:52 +0100

    Add test for RFC2181 5.2 We are currently failing this

---
+++ src/rna/packet/assemble_test.go
@@ -0,0 +1,27 @@
+package packet
+
+import (
+	"testing"
+	"fmt"
+)
+
+
+func TestRFC2181Sect52(t *testing.T) {
+	pp := &ParsedPacket{}
+
+	label, _ := ParseName([]byte{0})
+
+	pp.Answers = append(pp.Answers, ResourceRecordFormat{Name: label, Ttl: 30})
+	pp.Answers = append(pp.Answers, ResourceRecordFormat{Name: label, Ttl: 40})
+
+	raw := Assemble(pp)
+	parsed, err := Parse(raw)
+
+	if err != nil {
+		panic(err)
+	}
+
+	if parsed.Answers[0].Ttl != parsed.Answers[1].Ttl {
+		panic(fmt.Errorf("RRSet with different TTLs are forbidden"))
+	}
+}
